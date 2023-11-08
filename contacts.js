f6caeedb00326f5c65c1c2eae933f12b7cf2f1ba
Yuliia001 <babeyuliia@gmail.com>
2023-11-08 15:09:44 +0100

    fix bag

--- contacts.js
+++ contacts.js
@@ -10,13 +10,13 @@ export const listContacts = async () => {
 };
 
 export const getContactById = async (contactId) => {
-  const contacts = listContacts();
+  const contacts = await listContacts();
   const result = contacts.find((item) => item.id === contactId);
   return result || null;
 };
 
 export const removeContact = async (contactId) => {
-  const contacts = listContacts();
+  const contacts = await listContacts();
   const index = contacts.findIndex((item) => item.id === contactId);
   if (index === -1) {
     return null;
@@ -27,7 +27,7 @@ export const removeContact = async (contactId) => {
 };
 
 export const addContact = async ({ name, email, phone }) => {
-  const contacts = listContacts();
+  const contacts = await listContacts();
   const newContact = {
     id: nanoid(),
     name,
